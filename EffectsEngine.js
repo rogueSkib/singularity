59c41a2a0d60974529986387f9272c9e937c2a87
Jimmy Griffith <rogueSkib@gmail.com>
2015-11-15 22:56:13 -0800

    updates to EffectsEngine, WIP

--- EffectsEngine.js
+++ EffectsEngine.js
@@ -54,14 +54,14 @@ var OTHER_KEY_COUNT = OTHER_KEYS.length;
 
 
 
-exports = Class(View, function () {
+var EffectsEngine = Class(View, function () {
   var superProto = View.prototype;
 
-  this.init = function (opts) {
-    opts = opts || {};
-    opts.canHandleEvents = false;
-    opts.blockEvents = true;
-    superProto.init.call(this, opts);
+  this.init = function () {
+    superProto.init.call(this, {
+      canHandleEvents: false,
+      blockEvents: true
+    });
   };
 
   this.emitEffect = function (data) {
@@ -70,64 +70,18 @@ exports = Class(View, function () {
   };
 
   this.tick = function (dt) {
+    effectPool.forEachActive(function (effect) {
+      if (!effect.step(dt)) {
+        effect.recycle();
+      }
+    });
 
+    particlePool.forEachActive(function (particle) {
+      if (!particle.step(dt)) {
+        particle.recycle();
+      }
+    });
   };
-
-
-
-  /**
-   * internal use only
-   * clean-up a particle
-   *
-   * particle (this._ctor || ImageView)
-   * data (object)
-   * index (integer) position in this._activeParticleViews
-   */
-  this._killParticle = function (particle, data, index) {
-    var active = this._activeParticleViews;
-    var s = particle.style;
-    var spliced = active.splice(index, 1);
-
-    particle.pData = null;
-    data && data.onDeath && data.onDeath(particle, data);
-
-    s.visible = false;
-    this._freeParticleViews.push(spliced[0]);
-    data && this._freeParticleObjects.push(this._cleanObject(data));
-  };
-
-  /**
-   * finish and hide all particles immediately
-   */
-  this.killAllParticles = function () {
-    var active = this._activeParticleViews;
-    while (active.length) {
-      var particle = active[0];
-      this._killParticle(particle, particle.pData, 0);
-    }
-  };
-
-  /**
-   * public accessor for particle views (object data attached to each as pData)
-   */
-  this.getActiveParticles = function () {
-    return this._activeParticleViews;
-  };
-
-  /**
-   * fn (function) called for each active particle view, takes params: view, index
-   *
-   * ctx (object) the context on which fn should be called
-   *
-   * like Array.forEach, call a function for each active particle view
-   */
-  this.forEachActiveParticle = function (fn, ctx) {
-    var views = this._activeParticleViews;
-    for (var i = 0, len = views.length; i < len; i++) {
-      fn.call(ctx, views[i], i);
-    }
-  };
-
 });
 
 
@@ -137,7 +91,6 @@ var Effect = Class("Effect", function () {
     this.groupID = "";
     this.count = 0;
     this.continuous = false;
-    this.particles = [];
     this.data = null;
   };
 
@@ -145,7 +98,6 @@ var Effect = Class("Effect", function () {
     this.groupID = data.groupID || "";
     this.count = data.count || 1;
     this.continuous = data.continuous || false;
-    this.particles.length = 0;
     this.data = data;
 
     if (!this.continuous) {
@@ -156,20 +108,19 @@ var Effect = Class("Effect", function () {
   };
 
   this.recycle = function () {
-    for (var i = 0; i < this.particles.length; i++) {
-      this.particles[i].recycle();
-    }
     effectPool.release(this);
   };
 
   this.emitParticle = function () {
     var particle = particlePool.obtain();
     particle.reset(this.data);
-    this.particles.push(particle);
   };
 
   this.step = function (dt) {
+    // chance to emit each tick
+    if (this.continuous) {
 
+    }
   };
 });
 
@@ -179,13 +130,17 @@ var Particle = Class("Particle", function () {
   this.init = function () {
     this.view = new ImageView({ visible: false });
     this.currentImageURL = "";
+    this.isPolar = false;
+
     for (var i = 0; i < PROPERTY_KEY_COUNT; i++) {
       this[PROPERTY_KEYS[i]] = propertyPool.obtain();
     }
+
     for (var i = 0; i < OTHER_KEY_COUNT; i++) {
       var key = OTHER_KEYS[i];
       this[key] = OTHER_DEFAULTS[key];
     }
+
     this._poolIndex = 0;
     this._obtainedFromPool = false;
   };
@@ -216,28 +171,9 @@ var Particle = Class("Particle", function () {
       this.view.setImage(imageURL);
     }
 
-
-
+    // update isPolar flag
 
     // apply style properties
-    var s = this.view.style;
-    s.x = data.x;
-    s.y = data.y;
-    s.r = data.r;
-    s.anchorX = data.anchorX;
-    s.anchorY = data.anchorY;
-    s.width = data.width;
-    s.height = data.height;
-    s.scale = data.scale;
-    s.scaleX = data.scaleX;
-    s.scaleY = data.scaleY;
-    s.opacity = data.opacity;
-    s.flipX = data.flipX;
-    s.flipY = data.flipY;
-    s.compositeOperation = data.compositeOperation;
-
-
-
 
     if (this.delay === 0) {
       s.visible = true;
@@ -274,69 +210,11 @@ var Particle = Class("Particle", function () {
       return false;
     }
 
-
-
-
-    // translation
-    if (data.polar) {
-      data.radius += pct * data.dradius;
-      data.theta += pct * data.dtheta;
-      data.dradius += pct * data.ddradius;
-      data.dtheta += pct * data.ddtheta;
-      // allow cartesian translation of the origin point
-      data.ox += pct * data.dx;
-      data.oy += pct * data.dy;
-      data.dx += pct * data.ddx;
-      data.dy += pct * data.ddy;
-      // polar position
-      s.x = data.x = data.ox + data.radius * cos(data.theta);
-      s.y = data.y = data.oy + data.radius * sin(data.theta);
-    } else {
-      // cartesian by default
-      var dx = pct * data.dx;
-      if (dx !== 0) { s.x = data.x += dx; }
-      var dy = pct * data.dy;
-      if (dy !== 0) { s.y = data.y += dy; }
-      data.dx += pct * data.ddx;
-      data.dy += pct * data.ddy;
+    if (this.isPolar) {
+      // TODO: polar particle updates to x and y
     }
 
-    // anchor translation
-    var dax = pct * data.danchorX;
-    if (dax !== 0) { s.anchorX = data.anchorX += dax; }
-    var day = pct * data.danchorY;
-    if (day !== 0) { s.anchorY = data.anchorY += day; }
-    data.danchorX += pct * data.ddanchorX;
-    data.danchorY += pct * data.ddanchorY;
-
-    // stretching
-    var dw = pct * data.dwidth;
-    if (dw !== 0) { s.width = data.width += dw; }
-    var dh = pct * data.dheight;
-    if (dh !== 0) { s.height = data.height += dh; }
-    data.dwidth += pct * data.ddwidth;
-    data.dheight += pct * data.ddheight;
-
-    // rotation
-    var dr = pct * data.dr;
-    if (dr !== 0) { s.r = data.r += dr; }
-    data.dr += pct * data.ddr;
-
-    // scaling
-    var ds = pct * data.dscale;
-    if (ds !== 0) { s.scale = data.scale = max(0, data.scale + ds); }
-    var dsx = pct * data.dscaleX;
-    if (dsx !== 0) { s.scaleX = data.scaleX = max(0, data.scaleX + dsx); }
-    var dsy = pct * data.dscaleY;
-    if (dsy !== 0) { s.scaleY = data.scaleY = max(0, data.scaleY + dsy); }
-    data.dscale += pct * data.ddscale;
-    data.dscaleX += pct * data.ddscaleX;
-    data.dscaleY += pct * data.ddscaleY;
-
-    // opacity
-    var dop = pct * data.dopacity;
-    if (dop !== 0) { s.opacity = data.opacity = max(0, min(1, data.opacity + dop)); }
-    data.dopacity += pct * data.ddopacity;
+    // update delta properties
   };
 });
 
@@ -480,8 +358,9 @@ var ObjectPool = Class("ObjectPool", function () {
 
 
 
-// private class-wide pools
+// private class-wide pools and singleton exports
 var targetPool = new ObjectPool(Target);
 var propertyPool = new ObjectPool(Property);
 var particlePool = new ObjectPool(Particle);
 var effectPool = new ObjectPool(Effect);
+exports = new EffectsEngine();
